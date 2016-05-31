afdc6cab43e416a418743a7b0e39643d940b2f44
peterhuszti <peterhuszti4@gmail.com>
2016-05-31 12:27:27 +0200

    Some refactor and comment.

--- source/soe_seq/main.c
+++ source/soe_seq/main.c
@@ -21,7 +21,7 @@
 
 typedef uint64_t prime_t;
 typedef uint64_t word_t;
-#define LOG_WORD_SIZE 6
+#define LOG_WORD_SIZE 6 // 1 word = 2^(LOG_WORD_SIZE) i.e. 64 bit
 
 #define INDEX(i) ((i)>>(LOG_WORD_SIZE))
 #define MASK(i) ((word_t)(1) << ((i)&(((word_t)(1)<<LOG_WORD_SIZE)-1)))
@@ -35,22 +35,25 @@ typedef uint64_t word_t;
 /**
    Print/debug functions.
 */
-void print_table(size_t n, word_t *p){
-  for(size_t i=n; i>0; --i)
-    printf("%08lx ",p[i-1]);
-  printf("\n");
+void print_table(size_t n, word_t *p)
+{
+	for(size_t i=n; i>0; --i)
+		printf("%08lx ",p[i-1]);
+	printf("\n");
 }
 
-void print_primes(size_t nbits, word_t *st){
-  for(size_t i=0; i<nbits; i++) 
-    if(!GET(st,i)) printf("%lu, ", I2P(i));
-  printf("\n");
+void print_primes(size_t nbits, word_t *st)
+{
+	for(size_t i=0; i<nbits; i++) 
+		if(! GET(st,i)) printf("%lu, ", I2P(i));
+	printf("\n");
 }
 
-void print_primes_chunk(size_t nbits, word_t *st, size_t base){
-  for(size_t i=0; i<nbits; i++) 
-    if( ! GET( st, i ) ) printf( "%lu, ", I2P( i + base ) );
-  printf("\n");
+void print_primes_chunk(size_t nbits, word_t *st, size_t base)
+{
+	for(size_t i=0; i<nbits; i++)
+		if(! GET( st, i )) printf("%lu, ", I2P(i + base));
+	printf("\n");
 }
 
 
@@ -64,19 +67,23 @@ void print_primes_chunk(size_t nbits, word_t *st, size_t base){
    of bits allocated i.e. `nbits/8` bytes.
  */
 void soe_init(size_t nbits, word_t* st){
-  prime_t p = 3;
-  prime_t q = P2I(p);
-  
-  while(p * p < I2P(nbits) ) {
-    while(GET(st,q)) q++;
-    p = I2P(q);
-    prime_t i = P2I(p*p);
-    while(i<nbits){
-      SET(st,i);
-      i+=p;
-    }
-    q++;
-  }
+	prime_t p = 3; // first prime
+	prime_t q = P2I(p); // index in st
+
+	while(p * p < I2P(nbits)) // need to sieve only until sqrt(upper_bound)
+	{
+		while(GET(st,q)) q++; // search the next 0 in st, i.e. the next prime to sieve with
+
+		p = I2P(q); // what is this next sieving prime
+		prime_t i = P2I(p*p); // need to sieve only from p^2 because the smallers are already sieved
+
+		while(i < nbits) // sieve until it is in st
+		{
+			SET(st,i); // mark as composite
+			i += p; // step forward p
+		}
+		q++; // step forward 1, so the 2. while can find the next prime to sieve with
+	}
 }
 
 /**
@@ -85,9 +92,9 @@ void soe_init(size_t nbits, word_t* st){
 */
 inline prime_t negmodp2I(prime_t x, prime_t p)
 {
-  prime_t q = x % p;
-  q = q ? p-q : 0;
-  return q % 2 ? P2I(q + p) : P2I( q );
+	prime_t q = x % p;
+	q = q ? p-q : 0;
+	return q % 2 ? P2I(q + p) : P2I(q);
 }
 
 /**
@@ -106,64 +113,73 @@ inline prime_t negmodp2I(prime_t x, prime_t p)
    @param: `base` is the number of bits before the chunk (of course not
    allocated).
  */
-void soe_chunk(size_t nbits, word_t* st, 
-	       size_t chunk_bits, word_t* chunk, 
-	       prime_t base)
+void soe_chunk(size_t nbits, word_t* st,
+			   size_t chunk_bits, word_t* chunk, 
+			   prime_t base)
 {
-  for(size_t i = 1; i < nbits; ++i){ // start from 1 if 1 is in primes
-    if( ! GET(st,i) ){
-      prime_t p = I2P(i);
-      prime_t q = I2P( base );
-      assert(p<q);
-      q = negmodp2I( q, p );
-      while(q < chunk_bits){
-	SET(chunk, q);
-	q += p;
-      }
-    }
-  }
+	for(size_t i=1; i<nbits; ++i) // start from 1 if 1 is in primes
+	{
+		if(! GET(st,i)) // if it's a prime, then we sieve
+		{
+			prime_t p = I2P(i); // the prime in dec
+			prime_t q = I2P( base );  // the first number in the chunk
+			
+			assert(p<q);
+			q = negmodp2I( q, p ); // calculate offset
+			
+			while(q < chunk_bits) // while we are in the chunk
+			{
+				SET(chunk, q); // mark as composite
+				q += p; // next multiplier
+			}
+		}
+	}
 }
 
-int main(){
-  printf("Simple Sieve of Eratosthenese\n");
-  
-  /**
-     Allocate `st`.
-   */
-  size_t log_upper_bound = 13; 
-  size_t n = P2I(1<<(log_upper_bound-LOG_WORD_SIZE));
-  size_t nbits = n * sizeof(word_t) * CHAR_BIT;
-  word_t *st = (word_t*) calloc(sizeof(*st),n);
-  printf("Last candidate: %lu\n\n", I2P(nbits-1));
-  
-  soe_init(nbits, st);
-  // print_table(n, st);
-  // print_primes(nbits, st);
+int main()
+{
+	printf("Simple Sieve of Eratosthenese\n");
 
-  /* Initialization of chunks */
-  prime_t base = nbits;
-  size_t log_chunk_upper_bound = 10;
-  size_t chunk_size = 1<<(log_chunk_upper_bound - LOG_WORD_SIZE); 
-  size_t chunk_bits = chunk_size * sizeof(word_t) * CHAR_BIT;
+	/**
+	 Allocate `st`.
+	*/
+	size_t log_upper_bound = 13; 
+	size_t n = P2I(1<<(log_upper_bound-LOG_WORD_SIZE));
+	size_t nbits = n * sizeof(word_t) * CHAR_BIT;
+	word_t *st = (word_t*) calloc(sizeof(*st), n);
+	printf("Last candidate: %lu\n\n", I2P(nbits-1));
 
-  size_t num_chunks = 16;
-  word_t** chunks = (word_t**) malloc(sizeof(word_t*)*num_chunks);
-  word_t* chunk;
-  for( size_t i = 0; i < num_chunks; ++i ) {
-    chunks[i] = (word_t*) calloc( sizeof(word_t), chunk_size);
-  }
-  prime_t chunk_base = base;
-  for( size_t i = 0; i < num_chunks; ++i ) {
-    soe_chunk( nbits, st, chunk_bits, chunks[i], chunk_base);
-    print_primes_chunk( chunk_bits, chunks[i], chunk_base);
-    chunk_base += chunk_bits;
-  }
+	soe_init(nbits, st);
 
-  for(size_t i=0; i<num_chunks; i++) free(chunks[i]);
-  free(chunks);
-  free(st);
+	/* Initialization of chunks */
+	prime_t base = nbits;
+	size_t log_chunk_upper_bound = 10;
+	size_t chunk_size = 1<<(log_chunk_upper_bound - LOG_WORD_SIZE); 
+	size_t chunk_bits = chunk_size * sizeof(word_t) * CHAR_BIT;
 
-  printf("--- the end ---\n");
+	size_t num_chunks = 16;
+	word_t** chunks = (word_t**) malloc(sizeof(word_t*)*num_chunks);
+	word_t* chunk;
+	
+	for(size_t i=0; i<num_chunks; ++i) 
+	{
+		chunks[i] = (word_t*) calloc( sizeof(word_t), chunk_size);
+	}
+	
+	prime_t chunk_base = base;
+	
+	for(size_t i=0; i<num_chunks; ++i)
+	{
+		soe_chunk(nbits, st, chunk_bits, chunks[i], chunk_base);
+		print_primes_chunk(chunk_bits, chunks[i], chunk_base);
+		chunk_base += chunk_bits;
+	}
 
-  return 0;
+	for(size_t i=0; i<num_chunks; i++) free(chunks[i]);
+	free(chunks);
+	free(st);
+
+	printf("--- the end ---\n");
+
+	return 0;
 }
